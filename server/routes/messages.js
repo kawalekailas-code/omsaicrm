467d48845a8ae2f4cee44955f2f1d06c6d6f9948
kawalekailas-code <kawale.kailas@gmail.com>
2026-03-15 23:19:52 +0530

    Add files via upload

---
+++ server/routes/messages.js
@@ -0,0 +1,36 @@
+
+const router=require("express").Router()
+const axios=require("axios")
+const Message=require("../models/Message")
+
+router.post("/send",async(req,res)=>{
+
+ const {phone,text}=req.body
+
+ await axios.post(
+  `https://graph.facebook.com/v19.0/${process.env.PHONE_ID}/messages`,
+  {
+   messaging_product:"whatsapp",
+   to:phone,
+   type:"text",
+   text:{body:text}
+  },
+  {headers:{Authorization:`Bearer ${process.env.TOKEN}`}}
+ )
+
+ await Message.create({
+  phone,
+  text,
+  direction:"out",
+  type:"text"
+ })
+
+ res.send({status:"sent"})
+})
+
+router.get("/:phone",async(req,res)=>{
+ const msgs=await Message.find({phone:req.params.phone}).sort({createdAt:1})
+ res.send(msgs)
+})
+
+module.exports=router
