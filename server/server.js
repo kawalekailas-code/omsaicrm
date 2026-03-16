0503a997b9e9e997880d012004127d59bb567a04
kawalekailas-code <kawale.kailas@gmail.com>
2026-03-16 14:01:34 +0530

    Add files via upload

---
+++ server/server.js
@@ -0,0 +1,28 @@
+
+require("dotenv").config()
+const express=require("express")
+const mongoose=require("mongoose")
+const cors=require("cors")
+const bodyParser=require("body-parser")
+
+const app=express()
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(express.static("public"))
+app.use("/uploads",express.static("uploads"))
+
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+ console.log("MongoDB connected")
+})
+
+app.use("/contacts",require("./routes/contacts"))
+app.use("/messages",require("./routes/messages"))
+app.use("/media",require("./routes/media"))
+app.use("/templates",require("./routes/templates"))
+app.use("/campaign",require("./routes/campaign"))
+app.use("/webhook",require("./routes/webhook"))
+
+serverHttp.listen(process.env.PORT||3000,()=>{
+ console.log("CRM running")
+})
